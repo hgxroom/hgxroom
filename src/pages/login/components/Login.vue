6a3bbe3d9a6ad869cde35f57d658f6f61d59c002
redy <205823103@qq.com>
2022-03-17 20:56:28 +0800

    feat：登录页面

--- src/pages/login/components/Login.vue
+++ src/pages/login/components/Login.vue
@@ -1,126 +1,95 @@
 <template>
-  <t-form
-    ref="form"
-    :class="['item-container', `login-${type}`]"
-    :data="formData"
-    :rules="FORM_RULES"
-    label-width="0"
-    @submit="onSubmit"
-  >
-    <template v-if="type == 'password'">
-      <t-form-item name="account">
-        <t-input v-model="formData.account" size="large" placeholder="请输入账号：admin">
-          <template #prefix-icon>
-            <t-icon name="user" />
-          </template>
-        </t-input>
-      </t-form-item>
-
-      <t-form-item name="password">
-        <t-input
-          v-model="formData.password"
-          size="large"
-          :type="showPsw ? 'text' : 'password'"
-          clearable
-          placeholder="请输入登录密码：admin"
-        >
-          <template #prefix-icon>
-            <t-icon name="lock-on" />
-          </template>
-          <template #suffix-icon>
-            <t-icon :name="showPsw ? 'browse' : 'browse-off'" @click="showPsw = !showPsw" />
-          </template>
-        </t-input>
-      </t-form-item>
-
-      <div class="check-container remember-pwd">
-        <t-checkbox>记住账号</t-checkbox>
-        <span class="tip">忘记账号？</span>
-      </div>
-    </template>
-
-    <!-- 扫码登陆 -->
-    <template v-else-if="type == 'qrcode'">
-      <div class="tip-container">
-        <span class="tip">请使用微信扫一扫登录</span>
-        <span class="refresh">刷新 <t-icon name="refresh" /> </span>
-      </div>
-      <qrcode-vue value="" :size="192" level="H" />
-    </template>
-
-    <!-- 手机号登陆 -->
-    <template v-else>
-      <t-form-item class="verification-code" name="verifyCode">
-        <t-input v-model="formData.verifyCode" size="large" placeholder="请输入验证码" />
-        <t-button variant="outline" :disabled="countDown > 0" @click="handleCounter">
-          {{ countDown == 0 ? '发送验证码' : `${countDown}秒后可重发` }}
-        </t-button>
-      </t-form-item>
-    </template>
-
-    <t-form-item v-if="type !== 'qrcode'" class="btn-container">
-      <t-button block size="large" type="submit"> 登录 </t-button>
-    </t-form-item>
-
-    <div class="switch-container">
-      <span v-if="type !== 'password'" class="tip" @click="switchType('password')">使用账号密码登录</span>
-      <span v-if="type !== 'qrcode'" class="tip" @click="switchType('qrcode')">使用微信扫码登录</span>
-      <span v-if="type !== 'phone'" class="tip" @click="switchType('phone')">使用手机号登录</span>
+  <div class="login__form">
+    <div class="login__form__title">生产进度追溯系统</div>
+    <div class="login__form__content">
+      <t-form ref="form" :data="formData" :colon="true" :label-width="0" @reset="onReset" @submit="onSubmit">
+        <div class="login__form__text">用户名：</div>
+        <t-form-item name="account">
+          <t-input v-model="formData.account" clearable placeholder="请输入用户名">
+            <template #prefix-icon>
+              <desktop-icon />
+            </template>
+          </t-input>
+        </t-form-item>
+        <div class="login__form__text">密码：</div>
+        <t-form-item name="password">
+          <t-input v-model="formData.password" type="password" clearable placeholder="请输入密码">
+            <template #prefix-icon>
+              <lock-on-icon />
+            </template>
+          </t-input>
+        </t-form-item>
+        <t-form-item style="padding-top: 8px">
+          <t-button theme="primary" type="submit" block>登录</t-button>
+        </t-form-item>
+      </t-form>
     </div>
-  </t-form>
+  </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
-import QrcodeVue from 'qrcode.vue';
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
 import { MessagePlugin } from 'tdesign-vue-next';
-import { useCounter } from '@/hooks';
-import { useUserStore } from '@/store';
-
-const userStore = useUserStore();
+import { DesktopIcon, LockOnIcon } from 'tdesign-icons-vue-next';
 
 const INITIAL_DATA = {
-  phone: '',
-  account: 'admin',
-  password: 'admin',
-  verifyCode: '',
-  checked: false,
+  account: '',
+  password: '',
 };
 
-const FORM_RULES = {
-  phone: [{ required: true, message: '手机号必填', type: 'error' }],
-  account: [{ required: true, message: '账号必填', type: 'error' }],
-  password: [{ required: true, message: '密码必填', type: 'error' }],
-  verifyCode: [{ required: true, message: '验证码必填', type: 'error' }],
-};
+export default defineComponent({
+  components: {
+    DesktopIcon,
+    LockOnIcon,
+  },
+  setup() {
+    const formData = ref({ ...INITIAL_DATA });
 
-const type = ref('password');
+    const onReset = () => {
+      MessagePlugin.success('重置成功');
+    };
 
-const formData = ref({ ...INITIAL_DATA });
-const showPsw = ref(false);
+    const onSubmit = ({ validateResult, firstError }) => {
+      if (validateResult === true) {
+        MessagePlugin.success('提交成功');
+      } else {
+        console.log('Validate Errors: ', firstError, validateResult);
+        MessagePlugin.warning(firstError);
+      }
+    };
 
-const [countDown, handleCounter] = useCounter();
-
-const switchType = (val: string) => {
-  type.value = val;
-};
-
-const router = useRouter();
-
-const onSubmit = async ({ validateResult }) => {
-  if (validateResult === true) {
-    try {
-      await userStore.login(formData.value);
-
-      MessagePlugin.success('登陆成功');
-      router.push({
-        path: '/dashboard/base',
-      });
-    } catch (e) {
-      console.log(e);
-      MessagePlugin.error(e.message);
-    }
-  }
-};
+    return {
+      formData,
+      onReset,
+      onSubmit,
+    };
+  },
+});
 </script>
+<style lang="less">
+.login__form{
+  background: white;
+  width: 400px;
+  margin: 0 auto;
+  border-radius: 16px;
+  &__title {
+    font-size: 28px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 500;
+    color: rgba(0, 0, 0, 0.9);
+    line-height: 40px;
+    text-align: center;
+    padding: 40px 0px;
+  }
+  &__content {
+    margin: 0px 40px;
+    padding-bottom: 40px;
+  }
+  &__text{
+    font-size: 12px;
+    font-family: PingFangSC-Regular, PingFang SC;
+    font-weight: 400;
+    color: rgba(0, 0, 0, 0.6);
+    line-height: 40px;
+  }
+}
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,40 +1,33 @@
 <template>
   <div class="login-wrapper">
-    <login-header />
-
-    <div class="login-container">
-      <div class="title-container">
-        <h1 class="title margin-no">登录到</h1>
-        <h1 class="title">TDesign Starter</h1>
-        <div class="sub-title">
-          <p class="tip">{{ type == 'register' ? '已有账号?' : '没有账号吗?' }}</p>
-          <p class="tip" @click="switchType(type == 'register' ? 'login' : 'register')">
-            {{ type == 'register' ? '登录' : '注册新账号' }}
-          </p>
-        </div>
+    <div class="login__content">
+      <div class="login__icon">
+        <img src="./logo.png" />
       </div>
-
-      <login v-if="type === 'login'" />
-      <register v-else @register-success="switchType('login')" />
-      <tdesign-setting />
+      <login />
     </div>
-
-    <footer class="copyright">Copyright @ 2021-2022 Tencent. All Rights Reserved</footer>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-
 import Login from './components/Login.vue';
-import Register from './components/Register.vue';
-import LoginHeader from './components/Header.vue';
-import TdesignSetting from '@/layouts/setting.vue';
-
-const type = ref('login');
-const switchType = (val: string) => {
-  type.value = val;
-};
 </script>
 <style lang="less">
-@import url('./index.less');
+.login-wrapper {
+  height: 100vh;
+  background-size: cover;
+  background-position: 100%;
+  background: url("./login__background.png");
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login__icon {
+  width: 218px;
+  height: 64px;
+  margin: 0px auto;
+  img {
+    width: 100%;
+  }
+}
 </style>
