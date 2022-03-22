187a91b669776479056bac9b8e5eef052bfa3996
winf <15316139+winfcloud@users.noreply.github.com>
2022-03-22 11:47:03 +0800

    feat: 接口封装基础处理

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { getToken } from '@/utils/auth';
 import proxy from '../config/proxy';
 
 const env = import.meta.env.MODE || 'development';
@@ -11,15 +12,87 @@ const CODE = {
   REQUEST_FOBID: 1001,
 };
 
+/** 请求队列 */
+const pendingRequest = new Map();
+/** 生成请求key */
+function generateRequestKey(config) {
+  // const { method, url, params, data } = config
+  // const key = [method, url, JSON.stringify(params), JSON.stringify(data)].join('&')
+  // 不考虑携带参数
+  const { method, url } = config;
+  const key = [method, url].join('&');
+  return key;
+}
+/** 添加请求信息到队列 */
+function addPendingRequest(config, list) {
+  const requestKey = generateRequestKey(config);
+  config.cancelToken =
+    config.cancelToken ||
+    new axios.CancelToken((cancel) => {
+      if (!list.has(requestKey)) {
+        list.set(requestKey, cancel);
+      }
+    });
+}
+/** 检查是否存在重复请求，若存在则取消已发的请求 */
+function removePendingRequest(config, list) {
+  const requestKey = generateRequestKey(config);
+  if (list.has(requestKey)) {
+    const cancelToken = list.get(requestKey);
+    cancelToken(requestKey); // 把接口信息传下去，方便溯源，不影响取消
+    list.delete(requestKey);
+  }
+}
+
 const instance = axios.create({
   baseURL: host,
-  timeout: 1000,
+  timeout: 20000,
   withCredentials: true,
 });
 
-instance.interceptors.request.use((config) => config);
+instance.interceptors.request.use(
+  (config) => {
+    removePendingRequest(config, pendingRequest); // 检查是否存在重复请求，若存在则取消已发的请求
+    addPendingRequest(config, pendingRequest); // 把当前请求添加到pendingRequest对象中
 
-instance.defaults.timeout = 5000;
+    // 为每个请求都携带token  [`Token`]为自定义key，可以更改
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `bearer ${token}`;
+    }
+    // 请求类型处理
+    const method = config.method.toLowerCase();
+    switch (method) {
+      case 'get':
+        break;
+      case 'post':
+        break;
+      case 'formdata': {
+        // 上传文件特殊处理
+        const a = config.data;
+        const formData = new FormData();
+
+        for (const i in a) {
+          if (Object.hasOwnProperty.call(a, i)) {
+            const element = a[i];
+            formData.append(i, element);
+          }
+        }
+        config.data = formData;
+        config.timeout = 100000;
+        config.headers['Content-Type'] = 'multipart/form-data';
+        config.method = 'post';
+        break;
+      }
+      default:
+        break;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 instance.interceptors.response.use(
   (response) => {
