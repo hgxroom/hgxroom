20d52982349770645a1bc9367bbcdf309931dc4d
winf <15316139+winfcloud@users.noreply.github.com>
2022-04-11 11:53:44 +0800

    feat: 同步Starter0.2.2的多标签栏持久化功能

--- src/store/modules/permission.ts
+++ src/store/modules/permission.ts
@@ -46,6 +46,7 @@ export const usePermissionStore = defineStore('permission', {
 
       this.routers = accessedRouters;
       this.removeRoutes = removeRoutes;
+
       removeRoutes.forEach((item: RouteRecordRaw) => {
         if (router.hasRoute(item.name)) {
           router.removeRoute(item.name);
